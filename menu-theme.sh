cbbfd34d24e58f6c950178243137ca31120afb7a
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-02-20 22:52:02 +0800

    Update menu-theme.sh

--- menu-theme.sh
+++ menu-theme.sh
@@ -2,7 +2,7 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
-colornow=$(cat /etc/anggun/theme/color.conf)
+colornow=$(cat /etc/JsPhantom/theme/color.conf)
 NC="\e[0m"
 RED="\033[0;31m" 
 COLOR1="$(cat /etc/JsPhantom/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
