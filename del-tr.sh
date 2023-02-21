c94e2f1dd5a70b12cf796bf32bbda58bda1e604a
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-02-21 14:59:53 +0800

    Update del-tr.sh

--- del-tr.sh
+++ del-tr.sh
@@ -5,6 +5,13 @@
 # Auther  : JsPhantom
 # (C) Copyright 2023
 # =========================================
+colornow=$(cat /etc/JsPhantom/theme/color.conf)
+NC="\e[0m"
+RED="\033[0;31m" 
+COLOR1="$(cat /etc/JsPhantom/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+COLBG1="$(cat /etc/JsPhantom/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"   
+WH='\033[1;37m'                 
+###########- END COLOR CODE -##########
 clear
 red='\e[1;31m'
 green='\e[0;32m'
@@ -14,21 +21,21 @@ purple='\e[0;35m'
 NC='\e[0m'
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/usr/local/etc/xray/trojanws.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-        echo -e "\033[0;34m╔===========================================╗\033[0m"
-        echo -e "\e[0;35m           ${bold}Delete Xray Trojan WS Account   \033[0m"
-        echo -e "\033[0;34m╚===========================================╝\033[0m"
+        echo -e "$COLOR1╔===========================================╗\033[0m"
+        echo -e "$COLOR1 ${COLBG1}          ${WH}${bold}Delete Xray Trojan WS Account       \033[0m"
+        echo -e "$COLOR1╚===========================================╝\033[0m"
 		echo ""
 		echo "You have no existing clients!"
         echo ""
 		exit 1
 	fi
-        echo -e "\033[0;34m╔===========================================╗\033[0m"
-        echo -e "\e[0;35m'       ${bold}Delete Xray Trojan WS Account   \033[0m"
-        echo -e "\033[0;34m╚===========================================╝\033[0m"
+        echo -e "$COLOR1╔===========================================╗\033[0m"
+        echo -e "$COLOR1 ${COLBG1}       ${WH}${bold}Delete Xray Trojan WS Account       \033[0m"
+        echo -e "$COLOR1╚===========================================╝\033[0m"
 	
 	echo " Select the existing client you want to remove"
 	echo " Press CTRL+C to return"
-    echo -e "\033[0;34m===========================================\033[0m"
+    echo -e "$COLOR1============================================\033[0m"
 	echo "     No  Expired   User"
 	grep -E "^### " "/usr/local/etc/xray/trojanws.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
@@ -47,9 +54,9 @@ systemctl restart xray@trojanws.service
 systemctl restart xray@trnone.service
 clear
 echo " XRAY Trojan WS Account Deleted"
-echo -e "\033[0;34m===========================================\033[0m"
+echo -e "$COLOR1===========================================\033[0m"
 echo " Client Name : $user"
 echo " Expired On  : $exp"
-echo -e "\033[0;34m===========================================\033[0m"
+echo -e "$COLOR1===========================================\033[0m"
 echo -e ""
 echo -e "Autoscript By JsPhantom"
