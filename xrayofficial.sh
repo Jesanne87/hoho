336d82c6cb76eec36b8662ef6a3dcb9d70e822f0
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-02-24 22:43:50 +0800

    Update xrayofficial.sh

--- xrayofficial.sh
+++ xrayofficial.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #Script mod byJsPhantom
 #######################
-latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
-xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/xray-linux-64.zip"
+xraycore_link="https://github.com/annelyah23/xray/xray"
 echo ""
 echo -e "[ INFO ] Change Custom Xray-core"
 # Install New Xray Core Custom Method
 rm -rf /usr/local/bin/xray
-curl -sL "$xraycore_link" -o xray.zip
-unzip -q xray.zip && rm -rf xray.zip
+curl -sL "$xraycore_link" -o xray
 mv xray /usr/local/bin/xray
 chmod +x /usr/local/bin/xray
 systemctl restart xray
