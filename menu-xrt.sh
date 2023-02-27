9205b4cc20fa6237f760e622d41437d728a4309c
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-02-27 10:54:02 +0800

    Update menu-xrt.sh

--- menu-xrt.sh
+++ menu-xrt.sh
@@ -37,27 +37,27 @@ if [[ $(cat /opt/.ver) = $serverV ]] > /dev/null 2>&1; then
     if [[ $x -eq 1 ]]; then
        add-xrt
        read -n1 -r -p "Press any key to continue..."
-       menu
+       menu-xrt
     elif [[ $x -eq 2 ]]; then
        trial-xrt
        read -n1 -r -p "Press any key to continue..."
-       menu
+       menu-xrt
     elif [[ $x -eq 3 ]]; then
        cek-xrt
        read -n1 -r -p "Press any key to continue..."
-       menu
+       menu-xrt
     elif [[ $x -eq 4 ]]; then
        del-xrt
        read -n1 -r -p "Press any key to continue..."
-       menu
+       menu-xrt
     elif [[ $x -eq 5 ]]; then
        renew-xrt
        read -n1 -r -p "Press any key to continue..."
-       menu
+       menu-xrt
     elif [[ $x -eq 6 ]]; then
        user-xrt
        read -n1 -r -p "Press any key to continue..."
-       menu
+       menu-xrt
     elif [[ $x -eq 0 ]]; then
        clear
        menu-xray
