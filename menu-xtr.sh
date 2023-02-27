6111a4c874c1145d0dce4f652e7a4c33cccbb562
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-02-27 10:56:10 +0800

    Update menu-xtr.sh

--- menu-xtr.sh
+++ menu-xtr.sh
@@ -37,27 +37,27 @@ if [[ $(cat /opt/.ver) = $serverV ]] > /dev/null 2>&1; then
     if [[ $x -eq 1 ]]; then
        add-xtr
        read -n1 -r -p "Press any key to continue..."
-       menu
+       menu-xtr
     elif [[ $x -eq 2 ]]; then
        trial-xtr
        read -n1 -r -p "Press any key to continue..."
-       menu
+       menu-xtr
     elif [[ $x -eq 3 ]]; then
        cek-xtr
        read -n1 -r -p "Press any key to continue..."
-       menu
+       menu-xtr
     elif [[ $x -eq 4 ]]; then
        del-xtr
        read -n1 -r -p "Press any key to continue..."
-       menu
+       menu-xtr
     elif [[ $x -eq 5 ]]; then
        renew-xtr
        read -n1 -r -p "Press any key to continue..."
-       menu
+       menu-xtr
     elif [[ $x -eq 6 ]]; then
        user-xtr
        read -n1 -r -p "Press any key to continue..."
-       menu
+       menu-xtr
     elif [[ $x -eq 0 ]]; then
        clear
        menu-xray
